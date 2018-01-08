a1264876f9a7e455f899385839679d9383d124ba
ladyshaitan <hello.anne.pruett@gmail.com>
2018-01-08 15:16:03 -0800

    Code cleanup

--- index.js
+++ index.js
@@ -13,14 +13,9 @@ rl.question('Hi there!  Welcome to tic tac toe.  What\'s your first name?\nName:
   game.player1 = answer;
 });
 
-rl.question(`Hello, ${game.player1}!  What's the first name of player 2?`, (answer) => {
-  game.player2 = answer;
-
-  console.log(game);
-})
 
 rl.on('line', (input) => {
-
+  rl.close();
 
 });
 
